bb30e55b77c3805a621d22bf6b5ce424c8500792
Cascadia-JanetR <jrasque@hotmailcom>
2013-06-10 14:55:36 -0700

    Updating Code

--- js/capture1.js
+++ js/capture1.js
@@ -56,7 +56,22 @@ function onSuccess(imageURI) {
     image.src = imageURI;
 }
 
+// Show image
+//
+function cameraCallback(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
 
+{ quality : 75,
+  destinationType : Camera.DestinationType.DATA_URL,
+  sourceType : Camera.PictureSourceType.CAMERA,
+  allowEdit : true,
+  encodingType: Camera.EncodingType.JPEG,
+  targetWidth: 100,
+  targetHeight: 100,
+  popoverOptions: CameraPopoverOptions,
+  saveToPhotoAlbum: false };
 
 // retrieve supported image modes
 var imageModes = navigator.device.capture.supportedImageModes;
